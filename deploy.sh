29f1d66b9192bbcac7a8f5b1bbf3c6da3a4414c4
Ben Chan <benchan79@gmail.com>
2023-05-04 09:45:08 +0800

    Edit app name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker stop mod4-app || true
-docker rm mod4-app || true
+docker stop mod4-release-app || true
+docker rm mod4-release-app || true
 docker pull benchan79/mod4-project-docker:latest
-docker run -d --name mod4-app -p 8080:8080 benchan79/mod4-project-docker:latest
+docker run -d --name mod4-release-app -p 8080:8080 benchan79/mod4-project-docker:latest
